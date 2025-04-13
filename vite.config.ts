5bf9e340497663039efe6600b62e3f689f767c94
livaan <vanlinh30122003@gmail.com>
2025-04-13 23:55:09 +0700

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/vua_tieng_viet_003/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
